b745d56e96777606f69fe86fa7df70b40d7924eb
ykdai <YDAI005@e.ntu.edu.sg>
2024-05-26 13:59:10 +0000

    update download script

--- download_pretrained_models.py
+++ download_pretrained_models.py
@@ -5,12 +5,12 @@ from os import path as osp
 from basicsr.utils.download_util import load_file_from_url
 
 
-def download_pretrained_models(method, file_urls):
-    save_path_root = f'./ckpt/{method}'
+def download_pretrained_models(file_name, file_url):
+    save_path_root = f'./ckpt/'
+    save_path = f'./ckpt/{file_name}'
     os.makedirs(save_path_root, exist_ok=True)
 
-    for file_name, file_url in file_urls.items():
-        save_path = load_file_from_url(url=file_url, model_dir=save_path_root, progress=True, file_name=file_name)
+    save_path = load_file_from_url(url=file_url, model_dir=save_path_root, progress=True, file_name=save_path)
 
 
 if __name__ == '__main__':
